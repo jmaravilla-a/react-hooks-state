60826dbe3dfd400206adcea375b590180d1d80a7
Javier Maravilla <javier.maravilla@outlook.com>
2021-10-19 21:41:49 -0400

    commit tree 2e5f2329e938da604280f0ffe9bcd44053544d89

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState(true)
 
   function increment() {
-    setCount(count + 1);
+    setCount(count => ++count)
+    setColor(color => !color)
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  const background = color?'skyblue':'#ffdb58'
+
+  return <button onClick={increment} style={{background: background, fontSize: 450, color: "white"}}>{count}</button>;
 }
 
 export default Counter;
